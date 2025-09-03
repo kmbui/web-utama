3df070c22a7fc570c08c0e025ee52aca7c3b3e74
vissutagunawan <vglim3653@gmail.com>
2025-09-03 22:11:16 +0700

    add site constants

---
+++ src/lib/site.ts
@@ -0,0 +1,5 @@
+export const SITE_NAME =
+  "Keluarga Mahasiswa Buddhis Universitas Indonesia (KMBUI)";
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
+export const SITE_DESCRIPTION =
+  "Keluarga Mahasiswa Buddhis Universitas Indonesia (KMBUI) adalah organisasi kemahasiswaan Buddhis di Universitas Indonesia yang berfokus pada pengembangan spiritual, sosial, dan kebersamaan mahasiswa Buddhis.";
